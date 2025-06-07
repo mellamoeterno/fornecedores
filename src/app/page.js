08d7e14feb96c959f92551a1123af0766e6fb6e3
mellamoeterno <richmyself8@gmail.com>
2025-06-07 01:02:53 -0300

    addedPixelFromMeta

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,29 @@
 
 import React from 'react';
 import Link from 'next/link';
+import Script from 'next/script';
 
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center justify-center bg-[#410808]">     
+    <div className="w-full flex flex-col items-center justify-center bg-[#410808]"> 
+    <head>
+        {/* Meta Pixel Script */}
+        <Script id="facebook-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '569386696176592');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+      </head>    
       {/* Images */}      
       <div>
           <img src="/images/img1.png" className="w-full h-full object-cover" />
